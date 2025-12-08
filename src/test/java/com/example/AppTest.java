3db35b0bdab71d892b8f2f4538f1b8122b7362fe
duyang <451791599@qq.com>
2025-12-08 16:56:47 +0800

    1

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
+
